2267762f88c77f4cc76217f82b3d3e6a27ad11a8
brige.long <brige.long@cloudminds.com>
2023-03-28 19:42:27 +0800

    添加新的页面和路由

--- listen_books/lib/widget/widget_media_list_item.dart
+++ listen_books/lib/widget/widget_media_list_item.dart
@@ -1,45 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:listen_books/model/album.dart';
+import 'package:listen_books/utils/navigator_util.dart';
+import 'package:listen_books/utils/net_utils.dart';
+import 'package:listen_books/widget/widget_future_builder.dart';
 
 
 class WidgetMediaListItemPage extends StatelessWidget {
   const WidgetMediaListItemPage({super.key});
 
-  List<Widget> _getAllSonds() {
-    List listData = [{
-      "title":"标题内容1",
-      "author":"作者名称1",
-      "imageUrl": "http://pic.qqtn.com/up/2020-1/2020011418121854630.jpg"
-    },{
-      "title":"标题内容2",
-      "author":"作者名称2",
-      "imageUrl": "http://pic.qqtn.com/up/2020-1/2020011418121854630.jpg"
-    },{
-      "title":"标题内容3",
-      "author":"作者名称3",
-      "imageUrl": "http://pic.qqtn.com/up/2020-1/2020011418121854630.jpg"
-    },{
-      "title":"标题内容4",
-      "author":"作者名称4",
-      "imageUrl": "http://pic.qqtn.com/up/2020-1/2020011418121854630.jpg"
-    }];
+  Widget _getAllSonds(context, index, List<Album> dirs) {
+    return GestureDetector(
+      onTap: () {
+        print("click" + dirs[index].toString());
+        NavigatorUtil.goSongListPage(context);
+      },
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        width: 110,
+        height: 110,
+        // ignore: sort_child_properties_last
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 10),
+            Image.asset('assets/icon_dir_mp3.png',width: 110,height: 100,fit: BoxFit.fitWidth),
+            const SizedBox(height: 10),
+            Text(dirs[index].name, textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                )),
+          ],
+        ),
 
-    var tempList = listData.map((v){
-      return ListTile(
-        leading: Image.network(v['imageUrl']),
-        title: Text(v['title']),
-        subtitle: Text(v['author']),
-      );
-    });
-    return tempList.toList();
+        //绘制圆角背景
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            color: Colors.deepPurple,
+            border: Border.all(
+                color: Colors.deepPurple,
+                width: 1.0,
+            )
+        ),
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: ListView(
-        children: this._getAllSonds(),
-      ),
+    return CustomFutureBuilder<AlbumList>(
+      futureFunc: NetUtils.getAlbum,
+      builder: (context, snapshot) {
+        var data = snapshot.albums;
+        
+        var crossAxisCount = 2;
+        return  Container(
+          padding: const EdgeInsets.all(10),
+          child: GridView.builder(
+            itemCount: data.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: crossAxisCount,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 1.0
+            ),
+            itemBuilder: (context, index) {
+              return _getAllSonds(context, index, data);
+            }
+          ),
+        );
+      },
     );
   }
 }
